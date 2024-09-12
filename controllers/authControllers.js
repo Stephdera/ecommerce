f0811d80e303ed36aaddc3a77d39d9e817942597
Stephdera <ikwuegbustephanie56@gmail.com>
2024-09-12 14:55:38 +0100

    user

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -61,7 +61,7 @@ exports.register = async (req, res) => {
 
 exports.getUser = async ( req, res) => {
     try {
-        const user = await User.findById(req.user.id);
+        const user = await User.findById(req.user.id).populate();
         if (!user) {
             res.status(400).json({ message: "User does not exist"})
         }
